bf9ff14970eb042e4d198a6d20042c08290867e9
Victor Borja <vborja@apache.org>
2025-10-22 21:04:48 -0600

    add default template

--- flake.nix
+++ flake.nix
@@ -1,5 +1,9 @@
 {
   outputs = _: {
     flakeModule = ./nix/flakeModule.nix;
+    templates.default = {
+      path = ./templates/default;
+      description = "Minimal nixos configuration";
+    };
   };
 }
