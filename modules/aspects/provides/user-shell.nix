448a3b471eec3de08db7627cc499218df220cdaf
Victor Borja <vborja@apache.org>
2025-11-01 17:49:47 -0600

    feat(batteries): Add den.provides.user-shell

---
+++ modules/aspects/provides/user-shell.nix
@@ -0,0 +1,28 @@
+{
+  den._.user-shell = {
+    description = ''
+      Sets a user default shell, enables the shell at OS and home level.
+
+      Usage:
+
+        den.aspects.my-user._.user.includes = [
+          (den._.user-shell { shell = "fish"; })
+        ];
+    '';
+
+    __functor =
+      _:
+      { shell }:
+      { user, ... }:
+      {
+        homeManager.programs.${shell}.enable = true;
+        # Help needed: how to set default shell in darwin?
+        nixos =
+          { pkgs, ... }:
+          {
+            programs.${shell}.enable = true;
+            users.users.${user.userName}.shell = pkgs.${shell};
+          };
+      };
+  };
+}
