c0e2e5bdfd52521866177845000de711b2adb98a
Victor Borja <vborja@apache.org>
2025-11-02 00:10:03 -0600

    feat(batteries): Add den._.primary-user (#27)

---
+++ modules/aspects/provides/primary-user.nix
@@ -0,0 +1,32 @@
+{ lib, ... }:
+{
+  den._.primary-user.description = ''
+    Sets user as "primary".
+
+    On NixOS it adds whell group.
+    On Darwin it sets system.primaryUser
+    For WSL sets wsl.defaultUser if host has an `wsl` attribute.
+
+    ## Usage
+
+       den.aspects.my-user._.user.includes = [ 
+         (den._.primary-user)
+       ];
+  '';
+  den._.primary-user.__functor =
+    _:
+    { user, host }:
+    let
+      on-wsl.nixos.wsl.defaultUser = user.userName;
+    in
+    {
+      includes = lib.optionals (host ? wsl) [ on-wsl ];
+      darwin.system.primaryUser = user.userName;
+      nixos = {
+        users.users.${user.userName} = {
+          isNormalUser = true;
+          extraGroups = [ "wheel" ];
+        };
+      };
+    };
+}
