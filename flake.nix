23d578585c1da28adf179649ed6fc5b7f547dd7e
Victor Borja <vborja@apache.org>
2025-11-05 04:19:11 -0600

    move lib to nix and expose on flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,11 @@
 {
-  outputs = _: {
+  outputs = _inputs: {
     flakeModule = ./nix/flakeModule.nix;
     templates.default = {
       path = ./templates/default;
       description = "Minimal nixos configuration";
     };
     packages = import ./nix/template-packages.nix;
+    lib = import ./nix/lib.nix;
   };
 }
--- modules/lib.nix
+++ modules/lib.nix
@@ -1,44 +1,11 @@
 {
   lib,
   inputs,
-  config,
   ...
 }:
-let
-  # "Just Give 'Em One of These" -  Moe Szyslak
-  funk = import ../nix/aspect-functor.nix lib;
-
-  parametric =
-    param: aspect:
-    if param == true then
-      funk aspect
-    else
-      # deadnix: skip
-      { class, aspect-chain }: funk aspect param;
-
-  aspects = inputs.flake-aspects.lib lib;
-
-  # EXPERIMENTAL.  __findFile to resolve deep aspects.
-  #   __findFile = angleBrackets den.aspects;
-  #   <foo/bar/baz> => den.aspects.foo.provides.bar.provides.baz
-  # inspired by https://fzakaria.com/2025/08/10/angle-brackets-in-a-nix-flake-world
-  angleBrackets =
-    den-ns: _nixPath: name:
-    lib.pipe name [
-      (lib.replaceString "/" ".provides.")
-      (lib.splitString ".")
-      (path: lib.getAttrFromPath path den-ns)
-    ];
-in
 {
-  config.den.lib = {
-    inherit parametric aspects angleBrackets;
-
-    # default angle brackets search from den.aspects
-    __findFile = angleBrackets config.den.aspects;
-  };
+  config.den.lib = inputs.den.lib lib inputs;
   options.den.lib = lib.mkOption {
-    readOnly = true;
     internal = true;
     visible = false;
     type = lib.types.attrsOf lib.types.raw;
