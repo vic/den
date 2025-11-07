c792337830a63424db8b5f7fd1bb94d3877a2414
Victor Borja <vborja@apache.org>
2025-11-06 21:04:39 -0600

    create minimal template useful for cleaner start and bug reproduction. (#44)

---
+++ templates/minimal/flake.nix
@@ -0,0 +1,63 @@
+# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# Use `nix run .#write-flake` to regenerate it.
+{
+
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
+
+  inputs = {
+    den = {
+      url = "github:vic/den";
+    };
+    flake-aspects = {
+      url = "github:vic/flake-aspects";
+    };
+    flake-file = {
+      url = "github:vic/flake-file";
+    };
+    flake-parts = {
+      inputs = {
+        nixpkgs-lib = {
+          follows = "nixpkgs-lib";
+        };
+      };
+      url = "github:hercules-ci/flake-parts";
+    };
+    home-manager = {
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+      url = "github:nix-community/home-manager";
+    };
+    import-tree = {
+      url = "github:vic/import-tree";
+    };
+    nix-auto-follow = {
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+      url = "github:fzakaria/nix-auto-follow";
+    };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    nixpkgs-lib = {
+      follows = "nixpkgs";
+    };
+    systems = {
+      url = "github:nix-systems/default";
+    };
+    treefmt-nix = {
+      inputs = {
+        nixpkgs = {
+          follows = "nixpkgs";
+        };
+      };
+      url = "github:numtide/treefmt-nix";
+    };
+  };
+
+}
