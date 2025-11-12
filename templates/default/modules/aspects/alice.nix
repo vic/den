4cadf5e3b518c380817e57516a3668665a2d8679
Victor Borja <vborja@apache.org>
2025-11-12 11:13:54 -0600

    demo angle brackets in user aspect

--- templates/default/modules/aspects/alice.nix
+++ templates/default/modules/aspects/alice.nix
@@ -1,12 +1,15 @@
-{ den, eg, ... }:
+{ den, ... }:
 {
   den.aspects.alice = {
     # You can include other aspects, in this case some
     # den included batteries that provide common configs.
-    includes = [
-      eg.autologin
-      den.provides.primary-user # alice is admin always.
-      (den.provides.user-shell "fish") # default user shell
+    includes = let
+      # deadnix: skip # demo: enable <> on lexical scope
+      inherit (den.lib) __findFile;
+    in [
+      <eg/autologin>
+      <den/primary-user> # alice is admin always.
+      (<den/user-shell> "fish") # default user shell
     ];
 
     # Alice configures NixOS hosts it lives on.
