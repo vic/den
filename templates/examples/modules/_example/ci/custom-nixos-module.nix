0e94ddd82720de81032104997197b6f290d09310
Victor Borja <vborja@apache.org>
2025-11-13 02:23:38 -0600

    Added test from #84 for not having duplicated value set from den.default.
    Closes #84

--- templates/examples/modules/_example/ci/custom-nixos-module.nix
+++ templates/examples/modules/_example/ci/custom-nixos-module.nix
@@ -2,13 +2,22 @@
 let
   # A custom `nixos` class module that defines an option `names`.
   # Used to test that we are not duplicating values from owned configs.
-  nixosNames.options.names = lib.mkOption { type = lib.types.listOf lib.types.str; };
+  nixosNames = names: { options.${names} = lib.mkOption { type = lib.types.listOf lib.types.str; }; };
 in
 {
+  den.default.nixos.imports = [ (nixosNames "people") ];
+  den.default.includes = [
+    (
+      { user, ... }:
+      {
+        nixos.people = [ user.name ];
+      }
+    )
+  ];
 
   den.aspects.rockhopper.includes = [
     # Example: importing a third-party nixos module.
-    { nixos.imports = [ nixosNames ]; }
+    { nixos.imports = [ (nixosNames "names") ]; }
   ];
 
   den.aspects.rockhopper.nixos.names = [ "tux" ];
@@ -16,6 +25,9 @@ in
   perSystem =
     { checkCond, rockhopper, ... }:
     {
+      checks.rockhopper-default-people = checkCond "set from den.default for each user" (
+        rockhopper.config.people == [ "alice" ]
+      );
       checks.rockhopper-names-single-entry = checkCond "custom nixos array option set once" (
         rockhopper.config.names == [ "tux" ]
       );
