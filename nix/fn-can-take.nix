206fa197b06c1d03b8eab2f0135012bb35113fc0
Victor Borja <vborja@apache.org>
2025-11-18 15:15:49 -0600

    Fix parametric double inclusion on HM

--- nix/fn-can-take.nix
+++ nix/fn-can-take.nix
@@ -3,13 +3,14 @@ let
   check =
     params: func:
     let
+      givenFn = builtins.isFunction func || (builtins.isAttrs func && func ? __functor);
       givenArgs = builtins.isAttrs params;
       fargs = lib.functionArgs func;
       provided = builtins.attrNames params;
       args = lib.mapAttrsToList (name: optional: { inherit name optional; }) fargs;
       required = map (x: x.name) (lib.filter (x: !x.optional) args);
       intersection = lib.intersectLists required provided;
-      satisfied = givenArgs && lib.length required == lib.length intersection;
+      satisfied = givenFn && givenArgs && lib.length required == lib.length intersection;
       noExtras = lib.length required == lib.length provided;
       exactly = satisfied && noExtras;
     in
