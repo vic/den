a189c9f74bd2ff01f79e0d5e13c2c3bcb8265db2
Victor Borja <vborja@apache.org>
2026-02-01 02:16:19 -0600

    fix(noflake): Avoid user need to know of flake top-level attr.

--- templates/noflake/modules/den.nix
+++ templates/noflake/modules/den.nix
@@ -1,12 +1,38 @@
-{ inputs, lib, ... }:
+{ inputs, ... }:
 {
-  # we can import this flakeModule even if we dont
-  # have flake-parts as input!
-  imports = [ inputs.den.flakeModule ];
+  # tux user on igloo host.
+  den.hosts.x86_64-linux.igloo.users.tux = { };
 
-  # for flake.nixosConfigurations output.
-  # create a freeform option for it
-  options.flake = lib.mkOption {
-    type = lib.types.submodule { freeformType = lib.types.anything; };
+  # host aspect
+  den.aspects.igloo = {
+    nixos =
+      { pkgs, ... }:
+      {
+        # remove these for a real bootable host
+        boot.loader.grub.enable = false;
+        fileSystems."/".device = "/dev/fake";
+        passthru = { };
+
+        environment.systemPackages = [
+          pkgs.vim
+        ];
+      };
+  };
+
+  # user aspect
+  den.aspects.tux = {
+    # user configures the host it lives in
+    nixos = {
+      # hipster-tux uses nix-maid instead of home-manager.
+      imports = [ inputs.nix-maid.nixosModules.default ];
+
+      users.users.tux = {
+        isNormalUser = true;
+        maid.file.home.".gitconfig".text = ''
+          [user]
+            name=Tux
+        '';
+      };
+    };
   };
 }
