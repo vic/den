53bc73d78da4f39c3ffa9e8ab38ecea14ef12fcd
Victor Borja <vborja@apache.org>
2026-02-12 05:03:41 -0600

    update flakes (#157)

--- templates/ci/provider/flake.nix
+++ templates/ci/provider/flake.nix
@@ -2,7 +2,7 @@
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
     flake-parts.url = "github:hercules-ci/flake-parts";
     flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
     flake-aspects.url = "github:vic/flake-aspects";
--- templates/minimal/flake.nix
+++ templates/minimal/flake.nix
@@ -7,7 +7,7 @@
     }).config.flake;
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
     import-tree.url = "github:vic/import-tree";
     flake-aspects.url = "github:vic/flake-aspects";
     den.url = "github:vic/den";
