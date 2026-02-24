2f952347b602d0da13d510509c13e39b6f34ccdf
Victor Borja <vborja@apache.org>
2026-02-23 23:56:33 -0600

    user class modules can take os-level pkgs and osConfig. (#190)
    This is made possible via den.provides.forward now being able to use an
    adapter module and args adapter.

--- templates/ci/modules/features/os-user-class.nix
+++ templates/ci/modules/features/os-user-class.nix
@@ -14,5 +14,75 @@
       }
     );
 
+    test-forwards-os-args = denTest (
+      {
+        den,
+        igloo,
+        lib,
+        ...
+      }:
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+
+        den.aspects.tux.user =
+          { pkgs, ... }:
+          {
+            description = lib.getName pkgs.hello;
+          };
+
+        expr = igloo.users.users.tux.description;
+        expected = "hello";
+      }
+    );
+
+    test-forwards-mergeable-option = denTest (
+      {
+        den,
+        igloo,
+        lib,
+        ...
+      }:
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+
+        # via user class
+        den.aspects.tux.user =
+          { pkgs, ... }:
+          {
+            packages = [ pkgs.hello ];
+          };
+
+        # via user nixos
+        den.aspects.tux.nixos =
+          { pkgs, ... }:
+          {
+            users.users.tux.packages = [ pkgs.vim ];
+          };
+
+        # via host nixos
+        den.aspects.igloo.nixos =
+          { pkgs, ... }:
+          {
+            users.users.tux.packages = [ pkgs.tmux ];
+          };
+
+        expr = lib.sort (a: b: a < b) (
+          lib.filter (
+            name:
+            lib.elem name [
+              "hello"
+              "vim"
+              "tmux"
+            ]
+          ) (map lib.getName igloo.users.users.tux.packages)
+        );
+        expected = [
+          "hello"
+          "tmux"
+          "vim"
+        ];
+      }
+    );
+
   };
 }
