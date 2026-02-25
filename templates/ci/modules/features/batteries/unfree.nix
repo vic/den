024081e1d70adc874ebfe25a80c74bf6d35b9e8b
Oscar Marshall <3111765+OscarMarshall@users.noreply.github.com>
2026-02-24 16:15:44 -0800

    test(unfree): Improve test-user-class-works expr

--- templates/ci/modules/features/batteries/unfree.nix
+++ templates/ci/modules/features/batteries/unfree.nix
@@ -30,12 +30,8 @@
         den.hosts.x86_64-linux.igloo.users.tux = { };
         den.aspects.tux.includes = [ (den._.unfree [ "vscode" ]) ];
 
-        # In order to check that the user class hasn't broken, we force its
-        # evaluation through realizing `igloo.users.users.tux`. If it's broken,
-        # we'll see an error, so the expected value isn't important here -- it
-        # just needs to pass if there isn't an error.
-        expr = igloo.users.users.tux.name;
-        expected = "tux";
+        expr = !(igloo.users.users.tux ? unfree);
+        expected = true;
       }
     );
 
