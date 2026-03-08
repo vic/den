39e4a161ea616360b24c93854cffb1a5ab6c30e3
Victor Borja <vborja@apache.org>
2026-03-08 04:46:24 -0600

    ignore justfile

--- checkmate/modules/formatter.nix
+++ checkmate/modules/formatter.nix
@@ -2,6 +2,7 @@
   perSystem.treefmt.settings.global.excludes = [
     ".github/*TEMPLATE*/*"
     "docs/*"
+    "Justfile"
   ];
   perSystem.treefmt.programs.deadnix.enable = false;
   perSystem.treefmt.programs.nixf-diagnose.enable = false;
