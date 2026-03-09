2ac72ccd13ed43715f5663b63c46336399815067
Victor Borja <vborja@apache.org>
2026-03-09 10:04:24 -0600

    fix(battery): den._.hostname strictness

--- templates/ci/modules/features/batteries/hostname.nix
+++ templates/ci/modules/features/batteries/hostname.nix
@@ -29,5 +29,63 @@
       }
     );
 
+    test-included-in-host = denTest (
+      { den, igloo, ... }:
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+
+        den.aspects.igloo.includes = [ den._.hostname ];
+
+        expr = igloo.networking.hostName;
+        expected = "igloo";
+      }
+    );
+
+    test-included-in-user = denTest (
+      { den, igloo, ... }:
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+
+        den.aspects.tux.includes = [ den._.hostname ];
+
+        expr = igloo.networking.hostName;
+        expected = "igloo";
+      }
+    );
+
+    test-included-in-host-includes = denTest (
+      { den, igloo, ... }:
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+
+        # NOTE: foo needs parametric to pass over `{host}` context into den._.hostname
+        den.aspects.foo = den.lib.parametric {
+          includes = [ den._.hostname ];
+        };
+
+        den.aspects.igloo.includes = [ den.aspects.foo ];
+
+        expr = igloo.networking.hostName;
+        expected = "igloo";
+      }
+    );
+
+    test-included-in-user-includes = denTest (
+      { den, igloo, ... }:
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+
+        # NOTE: foo needs parametric to pass over `{host}` context into den._.hostname
+        den.aspects.foo = den.lib.parametric {
+          includes = [ den._.hostname ];
+        };
+
+        den.aspects.tux.includes = [ den.aspects.foo ];
+
+        expr = igloo.networking.hostName;
+        expected = "igloo";
+      }
+    );
+
   };
 }
