79ac3d0bf5ee56e5ef27d5f021b9e5d4dd0bbbf3
Victor Borja <vborja@apache.org>
2026-03-21 00:18:31 -0600

    example nvim config

--- templates/nvf-standalone/modules/den.nix
+++ templates/nvf-standalone/modules/den.nix
@@ -15,9 +15,98 @@
         {
           theme.enable = true;
           theme.name = "catppuccin";
-          theme.style = if mine then "latte" else "frappe";
+          theme.style = if mine then "frappe" else "latte";
+
+          extraPackages = [
+            pkgs.ripgrep
+            pkgs.fd
+            pkgs.fzf
+          ];
         };
+
+      includes = [ den.aspects.nvf ];
     };
+
+  den.aspects.nvf = {
+
+    includes = with den.aspects.nvf.provides; [
+      leader
+      keys
+      which-key
+      snacks
+      lazy
+    ];
+
+    provides.keys.vim.keymaps = [
+      {
+        key = "<leader><leader>";
+        mode = [ "n" ];
+        desc = "Smart find";
+        action = "function() Snacks.picker.smart() end";
+        lua = true;
+      }
+      {
+        key = "<leader>,";
+        mode = [ "n" ];
+        desc = "Buffers";
+        action = "function() Snacks.picker.buffers() end";
+        lua = true;
+      }
+      {
+        key = "<leader>/";
+        mode = [ "n" ];
+        desc = "Grep";
+        action = "function() Snacks.picker.grep() end";
+        lua = true;
+      }
+      {
+        key = "<leader>:";
+        mode = [ "n" ];
+        desc = "Command history";
+        action = "function() Snacks.picker.command_history() end";
+        lua = true;
+      }
+    ];
+
+    provides.leader.vim.globals = {
+      mapleader = " ";
+      maplocalleader = " ";
+    };
+
+    provides.which-key.vim.binds.whichKey = {
+      enable = true;
+      setupOpts = {
+        delay = 550;
+        preset = "helix";
+        keys = {
+          scroll_up = "<c-p>";
+          scroll_down = "<c-n>";
+        };
+      };
+    };
+
+    provides.lazy.vim =
+      { pkgs, ... }:
+      {
+        lazy.plugins."lazy.nvim".package = pkgs.vimPlugins.lazy-nvim;
+      };
+
+    provides.snacks.vim.utility.snacks-nvim = {
+      enable = true;
+      setupOpts = {
+        dashboard.enabled = true;
+        explorer.enabled = true;
+        input.enabled = true;
+        notifier.enabled = true;
+        picker.enabled = true;
+        quickfile.enabled = true;
+        scope.enabed = true;
+        scroll.enabled = true;
+        statuscolumn.enabled = true;
+        words.enabled = true;
+      };
+    };
+  };
 
   # Expose my-neovim app. Runnable with `nix run .#my-neovim`.
   # Adapt if you use flake-parts or whatever
