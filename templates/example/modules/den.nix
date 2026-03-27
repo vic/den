b18cfa1388595c194a72863c3efbe8ba493b3314
Victor Borja <vborja@apache.org>
2026-03-26 23:46:15 -0600

    restore hosts on example

--- templates/example/modules/den.nix
+++ templates/example/modules/den.nix
@@ -1,4 +1,7 @@
 {
-  den.ctx.user.classes = [ "homeManager" ];
+  den.hosts.x86_64-linux.igloo.users.alice = { };
+  den.hosts.aarch64-darwin.apple.users.alice = { };
   den.homes.x86_64-linux.alice = { };
+
+  den.ctx.user.classes = [ "homeManager" ];
 }
