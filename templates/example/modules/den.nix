63541487e9d866c5b43578e3a5fd62f6ba072e17
Victor Borja <vborja@apache.org>
2026-03-26 23:01:08 -0600

    Revert "fix(template): update default template to use mutual-providers" (#338)
    Reverts vic/den#337

--- templates/example/modules/den.nix
+++ templates/example/modules/den.nix
@@ -1,4 +1,5 @@
 {
-  den.ctx.user.classes = [ "homeManager" ];
+  den.hosts.x86_64-linux.igloo.users.alice.classes = [ "homeManager" ];
+  den.hosts.aarch64-darwin.apple.users.alice.classes = [ "homeManager" ];
   den.homes.x86_64-linux.alice = { };
 }
