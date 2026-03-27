bd1f36aa80d220ee8778a810c60a02a9c2231d55
Victor Borja <vborja@apache.org>
2026-03-26 23:48:48 -0600

    fmt

--- nix/lib/aspects/resolve.nix
+++ nix/lib/aspects/resolve.nix
@@ -4,18 +4,18 @@ let
   resolve =
     class: aspect-chain: aspect:
     let
-      provided =
-        if lib.isFunction aspect 
-        then aspect { inherit class aspect-chain; }
-        else aspect;
+      provided = if lib.isFunction aspect then aspect { inherit class aspect-chain; } else aspect;
 
       next-chain = aspect-chain ++ [ provided ];
 
-      imports = 
-        (lib.optional (provided ? ${class}) provided.${class}) ++
-        (map (resolve class next-chain) (provided.includes or []));
+      imports =
+        (lib.optional (provided ? ${class}) provided.${class})
+        ++ (map (resolve class next-chain) (provided.includes or [ ]));
 
-    in { inherit imports; };
+    in
+    {
+      inherit imports;
+    };
 
 in
 class: aspect: resolve class [ aspect ] aspect
