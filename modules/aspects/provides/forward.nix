8b0bfb375eca7a7b80295cdb657f64478974fc5a
Gelei <juinen@tuinberg.nl>
2026-03-29 06:16:59 +0200

    feat(core): support functionArgs in IntoPath (#342)
    Adds support for functionArgs inside the IntoPath arg of
    den.provides.forward.
    Includes tests for both home manager and nixos (might be a bit
    overkill?).
    Also fixes a typo in the documentation `permanance -> permanence`
    
    **usage example:**
    ```nix
      sysPersist =
        { host }:
        {
          class,
          aspect-chain,
        }:
        den._.forward {
          each = lib.singleton class;
          fromClass = _: "sysPersist";
          intoClass = _: host.class;
          intoPath =
            _:
            { config, ... }:
            [
              "environment"
              "persistence"
              config.impermanence.persistence-dir
            ];
          fromAspect = _: lib.head aspect-chain;
          guard = { options, ... }: options ? environment.persistence;
        };
    ```
    
    ---------
    
    Signed-off-by: Gelei <juinen@tuinberg.nl>
    Co-authored-by: Victor Borja <vborja@apache.org>

--- modules/aspects/provides/forward.nix
+++ modules/aspects/provides/forward.nix
@@ -51,6 +51,10 @@ let
       intoClass = fwd.intoClass item;
       intoPath = fwd.intoPath item;
 
+      intoPathArgs = if lib.isFunction intoPath then lib.functionArgs intoPath else { };
+      intoPathFn = if lib.isFunction intoPath then intoPath else _: intoPath;
+      staticIntoPath = if lib.isFunction intoPath then [ ] else intoPath;
+
       asp = fwd.fromAspect item;
       sourceModule = den.lib.aspects.resolve fromClass asp;
 
@@ -76,7 +80,7 @@ let
           fromClass
           intoClass
         ]
-        ++ intoPath
+        ++ staticIntoPath
       );
 
       guardArgs = if guard == null then { } else lib.functionArgs guard;
@@ -99,7 +103,7 @@ let
           ])
         ];
         ${intoClass} = {
-          __functionArgs = guardArgs;
+          __functionArgs = guardArgs // intoPathArgs;
           __functor = _: args: {
             options.den.fwd.${adapterKey} = lib.mkOption {
               default = { };
@@ -108,7 +112,7 @@ let
                 modules = adapterMods;
               };
             };
-            config = guardFn args (lib.setAttrByPath intoPath args.config.den.fwd.${adapterKey});
+            config = guardFn args (lib.setAttrByPath (intoPathFn args) args.config.den.fwd.${adapterKey});
           };
         };
       };
@@ -161,7 +165,8 @@ let
             evalImport args;
       };
 
-      needsAdapter = guard != null || adaptArgs != null || adapterModule != null;
+      needsAdapter =
+        guard != null || adaptArgs != null || adapterModule != null || builtins.isFunction intoPath;
       needsTopLevelAdapter = needsAdapter && intoPath == [ ];
       forwarded = forward intoPath;
 
---
+++ templates/ci/modules/features/dynamic-intopath.nix
@@ -0,0 +1,125 @@
+{ denTest, ... }:
+{
+  flake.tests.dynamic-intopath = {
+
+    test-dynamic-intoPath-host-scope = denTest (
+      {
+        den,
+        lib,
+        igloo,
+        ...
+      }:
+      let
+        slotMod =
+          { lib, ... }:
+          {
+            options.my-slot = lib.mkOption {
+              type = lib.types.str;
+              default = "slot-a";
+            };
+            options.my-box = lib.mkOption {
+              type = lib.types.attrsOf (
+                lib.types.submoduleWith {
+                  modules = [
+                    {
+                      config._module.freeformType = lib.types.lazyAttrsOf lib.types.anything;
+                    }
+                  ];
+                }
+              );
+              default = { };
+            };
+          };
+
+        forwarded =
+          { class, aspect-chain }:
+          den._.forward {
+            each = lib.singleton class;
+            fromClass = _: "src";
+            intoClass = _: "nixos";
+            intoPath =
+              _:
+              { config, ... }:
+              [
+                "my-box"
+                config.my-slot
+              ];
+            fromAspect = _: lib.head aspect-chain;
+          };
+      in
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+        den.aspects.igloo = {
+          includes = [ forwarded ];
+          nixos.imports = [ slotMod ];
+          nixos.my-slot = "slot-b";
+          src.my-data = "hello-from-src";
+        };
+
+        expr = igloo.my-box.slot-b.my-data;
+        expected = "hello-from-src";
+      }
+    );
+
+    test-dynamic-intoPath-user-scope = denTest (
+      {
+        den,
+        lib,
+        igloo,
+        ...
+      }:
+      let
+        slotMod =
+          { lib, ... }:
+          {
+            options.my-slot = lib.mkOption {
+              type = lib.types.str;
+              default = "slot-a";
+            };
+            options.my-box = lib.mkOption {
+              type = lib.types.attrsOf (
+                lib.types.submoduleWith {
+                  modules = [
+                    {
+                      config._module.freeformType = lib.types.lazyAttrsOf lib.types.anything;
+                    }
+                  ];
+                }
+              );
+              default = { };
+            };
+          };
+
+        forwarded =
+          { class, aspect-chain }:
+          den._.forward {
+            each = lib.singleton class;
+            fromClass = _: "src";
+            intoClass = _: "homeManager";
+            intoPath =
+              _:
+              { config, ... }:
+              [
+                "my-box"
+                config.my-slot
+              ];
+            fromAspect = _: lib.head aspect-chain;
+          };
+      in
+      {
+        den.hosts.x86_64-linux.igloo.users.tux = { };
+        den.aspects.igloo.homeManager.home.stateVersion = "25.11";
+        den.aspects.tux = {
+          includes = [ forwarded ];
+          homeManager.imports = [ slotMod ];
+          homeManager.my-slot = "slot-b";
+          src.my-data = "hello-from-src";
+        };
+
+        expr = igloo.home-manager.users.tux.my-box.slot-b.my-data;
+        expected = "hello-from-src";
+      }
+    );
+
+  };
+}
