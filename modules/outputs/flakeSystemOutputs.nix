96804c256de1ec4989aa28fc33b667c03cb4f272
Victor Borja <vborja@apache.org>
2026-03-31 12:22:41 -0600

    improve(core): Split outputs.nix into modules

---
+++ modules/outputs/flakeSystemOutputs.nix
@@ -0,0 +1,53 @@
+{
+  den,
+  lib,
+  options,
+  inputs,
+  ...
+}:
+let
+  ctx.flake.into.flake-system = _: map (system: { inherit system; }) den.systems;
+
+  systemOutput = output: { system }: lib.singleton { inherit system output; };
+
+  has-flake-output =
+    output: ((options.flake.type.getSubOptions or (_: options.flake)) { }) ? ${output};
+
+  systemOutputFwd =
+    { system, output }:
+    { class, aspect-chain }:
+    den._.forward {
+      each = lib.optional (class == "flake") output;
+      fromClass = _: output;
+      intoClass = _: "flake";
+      intoPath = _: [
+        "flake"
+        output
+        system
+      ];
+      guard = _: has-flake-output output;
+      adaptArgs = _: { pkgs = inputs.nixpkgs.legacyPackages.${system}; };
+      fromAspect = _: lib.head aspect-chain;
+    };
+
+  ctxSystemOuts =
+    let
+      outputs = [
+        "packages"
+        "apps"
+        "checks"
+        "devShells"
+        "legacyPackages"
+      ];
+
+      ctxs = map (output: {
+        flake-system.into."flake-${output}" = systemOutput output;
+        flake-system.provides."flake-${output}" = systemOutputFwd;
+      }) outputs;
+    in
+    ctxs;
+
+in
+{
+  den.ctx = lib.mkMerge (ctxSystemOuts ++ [ ctx ]);
+}
