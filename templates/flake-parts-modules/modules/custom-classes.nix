2402c56ff46367e7679e9b5b11a6362d0237ea9c
Michael Utz <michael@theutz.com>
2026-04-03 18:55:10 +0300

    docs: comment in flake-parts-module template about `systems` Follow-up to #368

--- templates/flake-parts-modules/modules/custom-classes.nix
+++ templates/flake-parts-modules/modules/custom-classes.nix
@@ -6,6 +6,12 @@
   ...
 }:
 {
+  # The flake-parts `systems` setting can be automatically generated
+  # from your host aspects.
+  #
+  # NOTE: If you're having trouble building this
+  # template on your machine, make sure you've created a host
+  # that matches your architecture in `./den.nix`.
   systems = builtins.attrNames den.hosts;
 
   # Some third-party flake-parts modules for demo purposes.
