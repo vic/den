038f2a8df5ab59d1e88d3a27fe8406bc4f654ee8
Victor Borja <vborja@apache.org>
2026-04-06 16:28:56 -0600

    aspect-meta

--- nix/lib/aspects/resolve.nix
+++ nix/lib/aspects/resolve.nix
@@ -3,21 +3,34 @@ let
 
   inherit (den.lib) canTake take;
 
+  apply =
+    provided: args:
+    let
+      res = if canTake.upTo args provided then take.upTo provided args else provided;
+      mod =
+        den.lib.aspects.types.aspectType.merge
+          [ ]
+          [
+            {
+              file = "<curried>";
+              value = res;
+            }
+          ];
+    in
+    if lib.isFunction res then mod else res;
+
   resolve =
     class: prev-chain: provided:
     let
-      aspect = if canTake.upTo args provided then take.upTo provided args else provided;
+      aspect = apply provided { inherit class aspect-chain; };
 
       aspect-chain = prev-chain ++ [ provided ] ++ (lib.optional (provided != aspect) aspect);
 
-      args = {
-        inherit aspect class aspect-chain;
-      };
+      classModule = lib.optional (aspect ? ${class}) (
+        lib.setDefaultModuleLocation "${class}@${aspect.name}" aspect.${class}
+      );
 
-      imports =
-        (lib.optional (aspect ? ${class}) aspect.${class})
-        ++ (map (resolve class aspect-chain) (aspect.includes or [ ]));
-
+      imports = classModule ++ (map (resolve class aspect-chain) (aspect.includes or [ ]));
     in
     {
       inherit imports;
---
+++ templates/ci/modules/features/aspect-meta.nix
@@ -0,0 +1,119 @@
+{ denTest, ... }:
+{
+  flake.tests.aspect-meta = {
+
+    test-meta-can-be-referenced = denTest (
+      { den, funnyNames, ... }:
+      {
+        den.aspects.foo = {
+          funny.names = [ den.aspects.foo.meta.nick ];
+          meta.nick = "McLoving";
+        };
+
+        expr = funnyNames den.aspects.foo;
+        expected = [ "McLoving" ];
+      }
+    );
+
+    test-meta-can-be-self-referenced = denTest (
+      { den, funnyNames, ... }:
+      {
+        den.aspects.foo =
+          { config, ... }:
+          {
+            funny.names = [ config.meta.nick ];
+            meta.nick = "McLoving";
+          };
+
+        expr = funnyNames den.aspects.foo;
+        expected = [ "McLoving" ];
+      }
+    );
+
+    test-name-can-be-referenced = denTest (
+      { den, funnyNames, ... }:
+      {
+        den.aspects.foo = {
+          funny.names = [ den.aspects.foo.name ];
+        };
+
+        expr = funnyNames den.aspects.foo;
+        expected = [ "den.aspects.foo" ];
+      }
+    );
+
+    test-name-can-be-self-referenced = denTest (
+      { den, funnyNames, ... }:
+      {
+        den.aspects.foo =
+          { config, ... }:
+          {
+            funny.names = [ config.name ];
+          };
+
+        expr = funnyNames den.aspects.foo;
+        expected = [ "den.aspects.foo" ];
+      }
+    );
+
+    test-meta-keys-at-host-aspect = denTest (
+      {
+        den,
+        igloo,
+        lib,
+        ...
+      }:
+      {
+        den.hosts.x86_64-linux.igloo = { };
+        den.aspects.igloo.includes = [ den.aspects.foo ];
+
+        den.aspects.foo =
+          { host }:
+          {
+            nixos.environment.sessionVariables = {
+              KEYS = lib.attrNames den.aspects.foo.meta.self.meta;
+            };
+          };
+
+        expr = {
+          inherit (igloo.environment.sessionVariables)
+            KEYS
+            ;
+        };
+        expected.KEYS = "file:loc:self";
+      }
+    );
+
+    test-meta-keys-at-host-fixpoint = denTest (
+      {
+        den,
+        igloo,
+        lib,
+        ...
+      }:
+      {
+        den.hosts.x86_64-linux.igloo = { };
+        den.aspects.igloo.includes = [ den.aspects.foo ];
+
+        den.aspects.foo =
+          { host }:
+          { class, aspect-chain }:
+          { config, lib, ... }:
+          {
+            nixos.environment.sessionVariables = {
+              KEYS = lib.attrNames config.meta;
+            };
+            meta.foo = 12;
+          };
+
+        expr = {
+          inherit (igloo.environment.sessionVariables)
+            KEYS
+            ;
+        };
+        expected.KEYS = "file:foo:loc:self";
+      }
+    );
+
+  };
+}
