809834982bb92e77228c91b9c24be8b17c06dfd3
Victor Borja <vborja@apache.org>
2026-04-07 09:17:09 -0600

    example feature detection between hosts

--- templates/ci/modules/features/resolve-adapters.nix
+++ templates/ci/modules/features/resolve-adapters.nix
@@ -51,5 +51,49 @@
         }
       );
 
+      test-host-conditional-aspect-inclusion = denTest (
+        {
+          den,
+          lib,
+          iceberg,
+          ...
+        }:
+        {
+          den.hosts.x86_64-linux.igloo = { };
+          den.hosts.x86_64-linux.iceberg = { };
+
+          den.aspects.foo.meta.key = "foo";
+          den.aspects.foo.nixos.environment.sessionVariables.message = "foo";
+
+          den.aspects.bar.nixos.environment.sessionVariables.message = "bar";
+
+          # host igloo includes foo
+          den.aspects.igloo.includes = [ den.aspects.foo ];
+
+          # host iceberg feature detects if foo is available at host igloo
+          den.aspects.iceberg.includes =
+            let
+              inherit (den.lib.aspects) resolve;
+              inherit (den.hosts.x86_64-linux) igloo;
+
+              iglooAspect = den.ctx.host { host = igloo; };
+              detectFoo = resolve.withAdapter (hasAspectWithKey "foo") "nixos" iglooAspect;
+
+              hasAspectWithKey =
+                key:
+                { aspect, recurse, ... }:
+                {
+                  found =
+                    aspect.meta.key or null == key || lib.any (i: (recurse i).found or false) (aspect.includes or [ ]);
+                };
+
+            in
+            lib.optional detectFoo.found den.aspects.bar;
+
+          expr = iceberg.environment.sessionVariables.message;
+          expected = "bar";
+        }
+      );
+
     };
 }
