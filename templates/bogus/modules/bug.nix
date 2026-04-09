328bf9a027c119793ad422c8659d839f655b9c01
horyzon <kalyan.coliveira@gmail.com>
2026-04-09 01:15:03 -0300

    BUG: attempts to use host as conditional of lib.optionalAttrs in sub aspect

--- templates/bogus/modules/bug.nix
+++ templates/bogus/modules/bug.nix
@@ -11,14 +11,48 @@
         ...
       }:
       {
-        # replace <system> if you are reporting a bug in MacOS
+
+        imports = let
+
+          # includes the subaspect named "sub" (which comes from the "foo" main
+          # aspect) INSIDE the "foo" main aspect itself, given that an "enable" option from the
+          # schema is satisfied
+          a = {
+            den.aspects.foo = { host, ... }: {
+              includes = lib.optionals (host.foo._.sub.enable == true) [
+                den.aspects.foo._.sub
+              ];
+            };
+          };
+
+          # declares that "enable" option in the schema
+          b = {
+            den.schema.host.options.foo._.sub.enable = lib.mkEnableOption "asdf";
+          };
+
+          # enables that "enable" in the schema
+          c = {
+            den.hosts.x86_64-linux.igloo.foo._.sub.enable = true;
+          };
+
+          # does a fancy thing in the sub aspect that I think was overlooked
+          # in https://github.com/vic/den/pull/410
+          d = {
+            den.aspects.foo._.sub = { host, ... }: {
+              nixos = lib.optionalAttrs (host.hostName != "whatever") {
+                networking.networkmanager.enable = true;
+              };
+            };
+          };
+
+        in [
+          a b c d
+        ];
+
         den.hosts.x86_64-linux.igloo.users.tux = { };
 
-        # do something for testing
-        den.aspects.tux.user.description = "The Penguin";
-
-        expr = igloo.users.users.tux.description;
-        expected = "The Penguin";
+        expr = igloo.networking.networkmanager.enable;
+        expected = true;
       }
     );
 
