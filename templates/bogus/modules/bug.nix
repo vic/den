e7e6dbcdac2105a494164d0eefe399f511b4baaa
horyzon <kalyan.coliveira@gmail.com>
2026-04-09 22:50:11 -0300

    BUG: static sub-aspects included from a parametric parent don't get their config applied

--- templates/bogus/modules/bug.nix
+++ templates/bogus/modules/bug.nix
@@ -1,26 +1,48 @@
-{ denTest, ... }:
+{ denTest, lib, ... }:
 {
   flake.tests.bogus = {
-
     test-something = denTest (
+      { den, igloo, ... }:
       {
-        den,
-        lib,
-        igloo, # igloo = nixosConfigurations.igloo.config
-        tuxHm, # tuxHm = igloo.home-manager.users.tux
-        ...
-      }:
-      {
-        # replace <system> if you are reporting a bug in MacOS
         den.hosts.x86_64-linux.igloo.users.tux = { };
 
-        # do something for testing
-        den.aspects.tux.user.description = "The Penguin";
+        imports =
+          let
+            a = {
+              den.schema.host.options.role._.sub.enable = lib.mkEnableOption "sub";
+            };
 
-        expr = igloo.users.users.tux.description;
-        expected = "The Penguin";
+            b = {
+              den.aspects.role = { host, ... }: {
+                includes = lib.optionals host.role._.sub.enable [
+                  den.aspects.role._.sub
+                ];
+              };
+            };
+
+            c = {
+              den.aspects.role._.sub.nixos.networking.networkmanager.enable = true;
+            };
+            # Change c to the below, and it works.
+            # c = {
+            #   den.aspects.role._.sub = { ... }: {
+            #     nixos.networking.networkmanager.enable = true;
+            #   };
+            # };
+
+            d = {
+              den.hosts.x86_64-linux.igloo.role._.sub.enable = true;
+            };
+
+            e = {
+              den.aspects.igloo.includes = [ den.aspects.role ];
+            };
+          in
+          [ a b c d e ];
+
+        expr = igloo.networking.networkmanager.enable;
+        expected = true;
       }
     );
-
   };
 }
